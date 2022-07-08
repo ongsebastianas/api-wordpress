e8e62bba11b3c96483e9fedd889235856563dc28
Ayrton <ayrtton.vinicius@gmail.com>
2022-07-08 07:49:13 -0300

    Add columns to the interested person CPT table

--- custom-post-types/interested-person.php
+++ custom-post-types/interested-person.php
@@ -9,9 +9,41 @@ function register_interested_person() {
         'rewrite' => array('slug' => 'interested_person', 'with_front' => true),
         'query_var' => true,
         'supports' => array('custom-fields'),
-        'publicly_queryable' => true,
+        'publicly_queryable' => false,
         'menu_icon' => 'dashicons-groups'
     ));
 }
 
 add_action('init', 'register_interested_person');
+
+// Set columns for custom fields
+function set_custom_interested_person_columns($columns) {
+    unset($columns['date']);
+    $columns['email'] = __('E-mail');
+    $columns['gender'] = __('Gênero');
+    $columns['verified_at'] = __('Data de verificação');
+
+    return $columns;
+}
+
+add_filter('manage_interested_person_posts_columns', 'set_custom_interested_person_columns');
+
+// Add the data to the custom columns
+function custom_interested_person_column($column, $post_id) {
+    switch ($column) {
+        case 'email':
+            echo get_post_meta($post_id, 'email', true);
+            break;
+
+        case 'gender':
+            echo ucfirst(get_post_meta($post_id, 'gender', true));
+            break;
+
+        case 'verified_at':
+            $verified_at = get_post_meta($post_id, 'verified_at', true);
+            echo $verified_at ? $verified_at : 'E-mail não verificado';
+            break;
+    }
+}
+
+add_action('manage_interested_person_posts_custom_column', 'custom_interested_person_column', 10, 2);
