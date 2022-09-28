98fa54919dbad0a50c7fa9657dce284f530ef345
Ayrton <ayrtton.vinicius@gmail.com>
2022-09-28 14:03:25 -0300

    Adjust email design

--- email-templates/verification-email.php
+++ email-templates/verification-email.php
@@ -21,7 +21,7 @@ function get_verification_email_template($token) {
                 </style>
             </head>
             <body>
-                <div style=\"background:rgb(98,143,111); padding: 60px 0;\">
+                <div style=\"background: url('http://projetosebastianas.com.br/wp-content/uploads/2022/09/design-3.png')rgb(98, 143, 111); background-repeat: no-repeat; background-size: cover; background-position: center; padding: 60px 0;\">
                     <table width=\"100%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\">
                         <tr>
                             <td align=\"center\">
@@ -29,7 +29,7 @@ function get_verification_email_template($token) {
                                     <div style=\"border-top: 13px solid #BF3604; border-radius: 1rem;\"></div>
 
                                     <div style=\"display: flex;\">
-                                        <div>
+                                        <div style=\"padding-bottom: 20px;\">
                                             <p style=\" color: #628F6F; padding: 2rem 2rem 0 2rem; text-align: left; font-size: 1.5rem; margin: 0;\">Olá!</p>
 
                                             <p style=\" color: #628F6F; padding: 0 2rem 2rem 2rem; text-align: left; font-size: 1.2rem; margin: 0;\">
@@ -37,14 +37,16 @@ function get_verification_email_template($token) {
                                                 no botão abaixo para verificarmos o seu e-mail.
                                             </p><br />
 
-                                            <a href=\"https://sebastianas.netlify.app/validationemail\" target=\"_Blank\" style=\"background: #BF3604; padding: 0.4rem 5rem 0.4rem 5rem; border-radius: 3.69rem; color: #fff; text-decoration: none; font-Weight: bold;\">Verificar</a><br /><br />
+                                            <div style=\"display: flex; justify-content: center;\">
+                                                <p style=\"background: #FFCFB4; margin: 0 auto 1.5rem; padding: 0.4rem 1rem 0.4rem 1rem; border-radius: 3rem; color:#BE3602; width: 14rem; border: 1px dashed #BE3602; font-Weight: bold; \">TOKEN: $token</p>
+                                            </div>
 
                                             <p style=\" color: #628F6F; padding: 2rem; text-align: left; font-size: 1.2rem; margin: 0;\">
                                                 Ah! E lembre-se de anotar o token que geramos especialmente pra você!
                                                 Você precisará dele para preencher o próximo formulário.
                                             </p>
 
-                                            <div style=\"background: #FFCFB4; margin: 0 0 1.5rem 2.4rem; padding: 0.4rem 1rem 0.4rem 1rem; border-radius: 3rem; color:#BE3602; width: 10rem; border: 1px dashed #BE3602; font-Weight: bold;\">TOKEN: $token</div>
+                                            <a href=\"https://sebastianas.netlify.app/validationemail\" target=\"_Blank\" style=\"background: #BF3604; padding: 0.5rem 5rem 0.5rem 5rem; border-radius: 3.69rem; color: #fff; text-decoration: none; font-weight: bold;\">Verificar</a><br /><br />
                                         </div>
                                     </div>
                                 </div>
